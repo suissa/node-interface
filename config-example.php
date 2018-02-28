4054a1fe7bf7152cb5a04cc9921c9907ac35ff18
Jean Carlo Nascimento <jnascimento@gmail.com>
2018-02-28 18:55:35 -0300

    Update config-example.php

--- config-example.php
+++ config-example.php
@@ -9,9 +9,9 @@ $nodeconfig = array(
     'pagetitle' => 'Litecoin Node Interface',
     'pagedesc' => '',
     'autorefresh' => 120,
-    'serverurl' => 'http://127.0.0.1:9332/',
-    'username' => 'rpcuser',
-    'password' => 'rpcpass',
+    'serverurl' => 'http://159.89.231.58:10000/',
+    'username' => 'litecoin',
+    'password' => 'd5b38bb46ab6b7647c9d0f35da',
     'broadcast' => false,
 );
 
